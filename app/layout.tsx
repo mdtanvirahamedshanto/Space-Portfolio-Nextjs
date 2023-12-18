53945a26c0ba6c6aa1cc9970e5a8b779e1160dce
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-18 13:09:14 +0600

    StartsCanva Backgroud

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import StarsCanva from "@/components/main/StarBackgroud";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <StarsCanva />
         {children}
       </body>
     </html>
