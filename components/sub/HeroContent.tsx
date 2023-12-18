2ecbce1524c3eead4522d7d93b63a02100019d42
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-18 12:39:16 +0600

    Hero Section Complete

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -17,7 +17,7 @@ const HeroContent = () => {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9] "
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] "
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5 " />
           <h1 className="Welcome-text text-[13px] ">
@@ -29,11 +29,14 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl text-blod text-white max-w-[600px] w-auto h-auto "
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
-            the best
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
+              {" "}
+              the best
+            </span>{" "}
+            project expreience
           </span>
-          Project Expreience
         </motion.div>
 
         <motion.p
@@ -45,7 +48,7 @@ const HeroContent = () => {
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg maxw-[200px]"
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] "
         >
           Learn More!
         </motion.a>
