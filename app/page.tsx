37d75dcd0934d00f47b799ccc5f4a6f9119ad50e
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-19 11:58:44 +0600

    Skill And Encription

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Encription from "@/components/main/Encription";
 import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col  gap-20 ">
         <Hero />
+        <Skills />
+        <Encription />
       </div>
     </main>
   );
