2a90ad36d66cafd2764163feb411ca047aa0b8e9
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-19 15:17:13 +0600

    Skills Added

--- components/main/Encription.tsx
+++ components/main/Encription.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Encription = () => {
   return (
-    <div className="flex flex-row relative items-center justify-center w-full h-full">
+    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
       <div className="absolute w-auto h-auto top-0 z-[5]">
         <motion.div
           variants={slideInFromTop}
@@ -36,6 +36,15 @@ const Encription = () => {
             height={70}
             className="z-10"
           />
+        </div>
+        <div className="Welcome-box px-[15px] py-[4px] z-[20] border my-[20px] border-[#7042f88b] opacity-[0.9]">
+          <h1 className="Welcome-text text-[12px]">Encryption</h1>
+        </div>
+      </div>
+
+      <div className="absolute z-[20] bottom-[10px] pz-[5px]">
+        <div className="cursive text-[20px] font-medium text-center text-gray-300">
+          Secure your data with end-to-end encryption
         </div>
       </div>
       <div className="w-full flex items-start justify-center absolute">
--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -12,7 +12,7 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20 pb-80 "
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20  "
       style={{ transform: "scale(0.9)" }}
     >
       <SkillText />
